b5cf528d6c0f4342bdab0fc86aa2d59df8ffc7a2
Omae Rio <rwek5514@icloud.com>
2025-04-17 13:23:49 +0900

    filamentとbreezeの導入・topページの設定

---
+++ backend/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>美原体育館協会</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+<h1>こちらは美原体育館協会Topページです</h1>
+</body>
+</html>
